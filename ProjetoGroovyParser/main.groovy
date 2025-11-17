03863835f911db9e0a963a38ed532eef088501c8
AlanGodoii <131309342+AlanGodoii@users.noreply.github.com>
2025-11-17 15:52:26 -0300

    Improve input handling with null check
    Added null check for input and exit on no input.

--- ProjetoGroovyParser/main.groovy
+++ ProjetoGroovyParser/main.groovy
@@ -1,15 +1,17 @@
-
-
 import core.Lexer
 import core.Parser
 
 println "Digite uma expressão matemática: "
-def input = System.console().readLine()
+def input = System.console()?.readLine()
 
-def tokens = Lexer.tokenize(input)
-def parser = new Parser()
+if (input == null) {
+    println "Nenhuma entrada lida."
+    System.exit(1)
+}
 
 try {
+    def tokens = Lexer.tokenize(input)
+    def parser = new Parser()
     def result = parser.parse(tokens)
     println "Resultado: $result"
 } catch (Exception e) {
